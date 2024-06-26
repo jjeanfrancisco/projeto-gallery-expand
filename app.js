1fc5cfa4f5b925ec0e276c0d7559b165ed56b196
DeveloperJean <74221819+jjeanfrancisco@users.noreply.github.com>
2024-06-26 12:59:45 -0300

    update

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-
-
-
+// Seleção dos elementos necessários
 const modal = document.querySelector(".modal");
 const previews = document.querySelectorAll(".gallery img");
 const original = document.querySelector(".full-img");
 const caption = document.querySelector(".caption");
 
-previews.forEach((preview) => {    
-    preview.addEventListener("click", () => {
-        modal.classList.add("open");
-        original.classList.add("open");
-        //Dynamic change text and image
-        const originalSrc = preview.getAttribute("data-original");
-        original.src = `${originalSrc}`;
-        const altText = preview.alt;
-        caption.textContent = altText;      
-    });    
+// Função para abrir o modal e atualizar o conteúdo
+const openModal = (preview) => {
+    modal.classList.add("open");
+    original.classList.add("open");
+
+    // Alterar dinamicamente o texto e a imagem
+    const { originalSrc, alt } = preview.dataset;
+    original.src = originalSrc;
+    caption.textContent = alt;
+};
+
+// Adicionar event listener a cada imagem da galeria
+previews.forEach(preview => {
+    preview.addEventListener("click", () => openModal(preview));
 });
 
+// Fechar o modal ao clicar fora da imagem
 modal.addEventListener("click", (e) => {
     if (e.target.classList.contains("modal")) {
         modal.classList.remove("open");
